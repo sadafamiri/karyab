036ed8078c44be7c779bf619628c972bb1ee14cf
sadafamiri <sadafamiri090@gmail.com>
2026-07-14 22:16:55 +0430

    kaaryab commit

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -1,26 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
 import Container from "@/components/layout/Container";
 import OpportunityCard from "@/components/opportunities/OpportunityCard";
 import { opportunities } from "@/data/opportunities";
+import SearchBar from "@/components/opportunities/SearchBar";
 
 export default function OpportunitiesPage() {
+  const [search, setSearch] = useState("");
   return (
     <section className="py-20">
       <Container>
         <h1 className="mb-10 text-center text-5xl font-bold">Opportunities</h1>
+        <SearchBar search={search} setSearch={setSearch} />
 
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-          {opportunities.map((opportunity) => (
-            <OpportunityCard
-              key={opportunity.id}
-              id={opportunity.id}
-              title={opportunity.title}
-              organization={opportunity.organization}
-              location={opportunity.location}
-              category={opportunity.category}
-              type={opportunity.type}
-              deadline={opportunity.deadline}
-            />
-          ))}
+          {opportunities
+            .filter((opportunity) =>
+              opportunity.title.toLowerCase().includes(search.toLowerCase()),
+            )
+            .map((opportunity) => (
+              <OpportunityCard
+                key={opportunity.id}
+                id={opportunity.id}
+                title={opportunity.title}
+                organization={opportunity.organization}
+                location={opportunity.location}
+                category={opportunity.category}
+                type={opportunity.type}
+                deadline={opportunity.deadline}
+              />
+            ))}
         </div>
       </Container>
     </section>
---
+++ components/opportunities/SearchBar.tsx
@@ -0,0 +1,18 @@
+type SearchBarProps = {
+  search: string;
+  setSearch: (value: string) => void;
+};
+
+export default function SearchBar({ search, setSearch }: SearchBarProps) {
+  return (
+    <div className="mb-8">
+      <input
+        type="text"
+        placeholder="Search opportunities..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-full rounded-lg border border-gray-300 p-3 outline-none focus:border-blue-500"
+      />
+    </div>
+  );
+}
